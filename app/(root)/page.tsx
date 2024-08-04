37914614741c4419eba2c812e354b12a9b760e2d
John Mark Delima <delimajohnmark14@gmail.com>
2024-08-04 18:02:24 +0800

    Display all the documents on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import AddDocumentBtn from '@/components/AddDocumentBtn';
-import Header from '@/components/Header';
-import { SignedIn, UserButton } from '@clerk/nextjs';
-import { currentUser } from '@clerk/nextjs/server';
-import Image from 'next/image';
-import { redirect } from 'next/navigation';
-
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import { getDocuments } from "@/lib/actions/room.actions";
+import { dateConverter } from "@/lib/utils";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -13,7 +15,10 @@ const Home = async () => {
     redirect("/sign-in");
   }
 
-  const documents = [];
+  const roomDocuments = await getDocuments(
+    clerkUser.emailAddresses[0].emailAddress
+  );
+
   return (
     <main className="home-container">
       <Header className="sticky left-0 top-0">
@@ -25,8 +30,32 @@ const Home = async () => {
         </div>
       </Header>
 
-      {documents.length > 0 ? (
-        <div></div>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({id, metadata, createdAt}: any) => (
+              <li key={id} className="document-list-item">
+                <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                  <Image src={"/assets/icons/doc.svg"} alt="file" width={40} height={40} />
+                </div>
+                <div className="space-y-1">
+                  <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                  <p className="text sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
+                </div>
+                </Link>
+                {/* TODO: Delete button */}
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <div className="document-list-empty">
           <Image
@@ -44,6 +73,6 @@ const Home = async () => {
       )}
     </main>
   );
-}
+};
 
-export default Home
+export default Home;
